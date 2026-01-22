d81c55b6ab0c7e9f69b2d5b7f29a0c66da44dfdd
Evander <rojasevander@gmail.com>
2026-01-21 23:54:57 -0500

    handle password

--- app/controllers/users.py
+++ app/controllers/users.py
@@ -7,17 +7,20 @@ users_bp = Blueprint("users", __name__)
 @users_bp.route("/", methods=["GET"])
 def list_users():
     users = User.query.all()
-    return jsonify([{"id": u.id, "name": u.name, "email": u.email} for u in users])
+    return jsonify([{"id": u.id, "username": u.username, "email": u.email} for u in users])
 
 @users_bp.route("/", methods=["POST"])
 def create_user():
     data = request.get_json() or {}
-    name = data.get("name")
+    username = data.get("username")
     email = data.get("email")
-    if not name or not email:
-        return jsonify({"error": "name and email are required"}), 400
+    pwd = data.get("password")
 
-    user = User(name=name, email=email)
+    if not username or not email:
+        return jsonify({"error": "username and email are required"}), 400
+
+    user = User(username=username, email=email)
+    user.set_password(pwd)
     db.session.add(user)
     db.session.commit()
     return jsonify({"message": "User created", "id": user.id}), 201
